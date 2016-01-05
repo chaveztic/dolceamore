6d1a5f1486efe2423600e2923dc5e6f92dfea52e
ChavezTIC <chavez_048@hotmail.com>
2016-01-05 11:29:27 -0600

    blade, intervention y mercadopago

--- config/app.php
+++ config/app.php
@@ -156,6 +156,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Collective\Html\HtmlServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class
     ],
 
     /*
@@ -201,7 +203,11 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
+        'Image'     => Intervention\Image\Facades\Image::class
     ],
 
 ];
